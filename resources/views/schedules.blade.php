6b2365ac50afbee1e72bed6743f3d0b397408bbc
Nathan Darmawan <hoboman1234567890@gmail.com>
2025-01-13 14:55:28 +0700

    margin fix

--- resources/views/schedules.blade.php
+++ resources/views/schedules.blade.php
@@ -6,7 +6,7 @@
 @endsection
 @section('content')
 
-<div class="container mt-5">
+<div class="container mt-5 mb-5">
     <div class="row">
         <div class="col-md-3">
             <form method="POST" action="{{ route('schedules.changeStage') }}">
